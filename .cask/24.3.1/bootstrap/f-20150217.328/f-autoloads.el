109371c31c779ec10e0de59917bd1f82f4009a2e
remerci <heihe_yellow@163.com>
2015-04-05 23:40:47 +0800

    first

---
+++ .cask/24.3.1/bootstrap/f-20150217.328/f-autoloads.el
@@ -0,0 +1,18 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21755 2151 636426
+;;;;;;  18000))
+
+;;;***
+
+(provide 'f-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; f-autoloads.el ends here
